38161221dc4b96255fbf7da23f34aa8e5b767bd7
Marina <marinaflomar@gmail.com>
2024-06-22 07:22:18 +0200

    blabla

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 import peliculas from './peliculas.js'
 
+const genres = {
+    28: 'genero-28', 
+    53: 'genero-53', 
+    12: 'genero-12'  
+};
+
+const addMovieToDOM = (movie, genreId) => {
+    const container = document.getElementById(genres[genreId]);
+    const texto = document.createElement('h4');
+    const img = document.createElement('img');
+    const contenedor = document.createElement('div');
+
+    texto.innerText = movie.title;
+    img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+    contenedor.appendChild(img);
+    contenedor.appendChild(texto);
+    container.appendChild(contenedor);
+};
+
+
+peliculas.forEach(pelicula => {
+    pelicula.genre_ids.forEach(genreId => {
+        if (genres[genreId]) {
+            addMovieToDOM(pelicula, genreId);
+        }
+    });
+});
